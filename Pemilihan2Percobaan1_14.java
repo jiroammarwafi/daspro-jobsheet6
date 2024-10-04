465f0a33c6458c6b4325d5eb78cb4f6937e81f4e
jiroammarwafi <149659337+jiroammarwafi@users.noreply.github.com>
2024-10-04 10:36:50 +0700

    Modifikasi 1

--- Pemilihan2Percobaan1_14.java
+++ Pemilihan2Percobaan1_14.java
@@ -8,10 +8,13 @@ public class Pemilihan2Percobaan1_14 {
     tahun = sc.nextInt();
 
     if ((tahun % 4) == 0) {
-        if ((tahun % 100) != 0)
-            System.out.println("Tahun Kabisat");
-    } else
-        System.out.println("Bukan Tahun Kabisat");
-
+        if ((tahun % 100) != 0) {
+                System.out.println("Tahun Kabisat");
+            } else {
+                System.out.println("Bukan Tahun Kabisat");
             }
+        } else {
+            System.out.println("Bukan Tahun Kabisat 1");
+        }
+    }
 }
